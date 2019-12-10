4efde6f382588a3666893d3cf2244d7bc196db71
Dmytrii Puzyr <slymahel@gmail.com>
2019-12-10 15:22:06 +0200

    Add useless variables

--- calculator.cpp
+++ calculator.cpp
@@ -12,5 +12,18 @@ int Calculator::Sub (double a, double b)
 
 int Calculator::Mul (double a, double b)
 {
+    int a1 = 1;
+    int a2 = 1;
+    int a3 = 1;
+    int a4 = 1;
+    int a5 = 1;
+    int a6 = 1;
+    int a7 = 1;
+    int a8 = 1;
+    int a9 = 1;
+    int a10 = 1;
+    int a11 = 1;
+
+
     return a * b + 0.5;
 }
